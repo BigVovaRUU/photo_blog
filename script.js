6c794efd39e478524100793272c14ef4e6c7dff2
Владимир <w@bigvova.com>
2025-09-28 00:34:39 +0300

    Обновление файлов
    обновление

---
+++ script.js
@@ -0,0 +1,22 @@
+// Tailwind config
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#364049",
+        "background-light": "#f7f7f7",
+        "background-dark": "#17191b",
+      },
+      fontFamily: {
+        display: ["Public Sans"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px",
+      },
+    },
+  },
+};
